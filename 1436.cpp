010083e61b4636dcf0431eb9ef0806226c2a867c
Ryan Hwang <hsw0194@gmail.com>
2020-06-12 23:58:55 +0900

    Upload June 12 #2 algorithm

---
+++ 1436.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ans(int);
+
+int main(void) {
+  int num;
+  cin >> num;
+  cout << ans(num);
+}
+
+int ans(int num) {
+  int cnt = 0;
+  int title_num = 1;
+  while (title_num++) {
+    string s = to_string(title_num);
+    if (s.find("666") != -1) cnt++;
+    if (cnt == num)
+      return title_num;
+    else
+      continue;
+  }
+}
