b638febd16d19c7d3d9e003d292334d5314ce845
Ryan Hwang <hsw0194@gmail.com>
2020-06-29 17:12:41 +0900

    Modify 1946 solution to make faster

--- 1946.cpp
+++ 1946.cpp
@@ -26,14 +26,26 @@ int main(void) {
 }
 
 int ans() {
-  pass = N;
+  pass = 1;
+  int tmp_rank=meet_case[1];
 
-  for (int i = 1; i <= N; i++) {
-    for (int j = i; j > 0; j--) {
-      if (meet_case[i] > meet_case[j]) {
-        pass--;
-        break;
-      }
+ // 기존 코드 사용하면 TLE 위험 있음
+  // for (int i = 1; i <= N; i++) {
+  //   for (int j = i; j > 0; j--) {
+  //     if (meet_case[i] > meet_case[j]) {
+  //       pass--;
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // 기존에 합격한 사람보다 면접순위가 더 높으면 그사람은
+  // 서류에서 밀리더라도 (i가 1에서부터 쭉 증가하니 )
+  // 최종합격 (면접 순위가 밀리지 않으니)
+   for (int i = 2; i <= N; i++) {
+    if(meet_case[i]<tmp_rank) {
+      pass++;
+      tmp_rank=meet_case[i];
     }
   }
 
