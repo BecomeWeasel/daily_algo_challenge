1ed45221bc8d60f66eab8b6dc338db05e08f321b
BecomeWeasel <hsw0194@gmail.com>
2020-07-10 00:48:40 +0900

    Upload july 10 algorithm(easy dp)

---
+++ 11726.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define N_MAX 1001
+
+int n;
+int d[N_MAX];
+
+int ans();
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> n;
+  cout << ans();
+}
+
+int dp(int target_n) {
+  if (target_n == 1) return 1;
+  if (target_n == 2) return 2;
+  if (target_n == 3) return 3;
+  if (d[target_n] != 0) return d[target_n];
+  return d[target_n] = (dp(target_n - 2) + dp(target_n - 1))%10007;
+}
+
+int ans() { return dp(n); }
