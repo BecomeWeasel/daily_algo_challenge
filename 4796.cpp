e2bd75c1fc026f2bf4bf9ef0c5cd4f8ebbfc22ff
BecomeWeasel <hsw0194@gmail.com>
2020-07-13 17:29:50 +0900

    Upload july 13 #3 algorithm(easy greedy)

---
+++ 4796.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int ans();
+
+int L, P, V;
+
+int main(void) {
+  int i = 1;
+  while (true) {
+    cin >> L >> P >> V;
+    if (L * P * V == 0) break;
+    cout << "Case " << i++ << ": " << ans() << '\n';
+  }
+}
+
+int ans() {
+  int i = (V/P*P)+1;
+  int cnt = V/P*L;
+  int conseq_night=V%P>L?L:V%P;
+
+  
+  return cnt+conseq_night;
+}
