dd44dfe1dc343850522f9bf9395c3b0a5f4e030c
BecomeWeasel <hsw0194@gmail.com>
2020-08-12 14:07:30 +0900

    Upload august 12 algorithm(prime number,bfs)

---
+++ 2023.py
@@ -0,0 +1,46 @@
+from sys import stdin
+from collections import deque
+
+
+def isPrime(n):
+  if n < 2: return False
+  i = 2
+  while i * i <= n:
+    if n % i == 0: return False
+    i += 1
+  return True
+
+
+def bfs():
+  q = deque()
+  while result_list:
+    q.append(result_list.popleft())
+
+  while q:
+    number = q.popleft()
+    i = 1
+    while i < 10:
+      if isPrime(number * 10 + i):
+        result_list.append(number * 10 + i)
+
+      i += 2
+
+
+def ans():
+  for digit in range(1, N):
+    bfs()
+
+  for i in result_list:
+    print(i)
+
+
+result_list = deque()
+
+result_list.append((2))
+result_list.append((3))
+result_list.append((5))
+result_list.append((7))
+
+N = int(stdin.readline())
+
+ans()
