357c5108c151141294d7d98fd81f4b823a945f79
BecomeWeasel <hsw0194@gmail.com>
2020-08-27 16:33:15 +0900

    Upload august 27 #2 algorithm(easy DS,queue)

---
+++ 2161.py
@@ -0,0 +1,27 @@
+from sys import stdin
+from collections import deque
+
+N = int(stdin.readline())
+
+
+def ans():
+  q = deque()
+  print_list = list()
+  for i in range(1, N + 1):
+    q.append(i)
+
+  while len(q) != 1:
+    trash = q.popleft()
+    print_list.append(trash)
+
+    top = q.popleft()
+    q.append(top)
+
+  s = ""
+  for c in print_list:
+    s += str(c) + " "
+  s += str(q.pop())
+  print(s)
+
+
+ans()
