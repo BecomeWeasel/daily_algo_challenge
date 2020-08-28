bc2281ec43a180efe8d3b7bf91a0885e3ab70bea
BecomeWeasel <hsw0194@gmail.com>
2020-08-28 09:39:18 +0900

    Upload august 28 algorithm(DS,hash)

---
+++ 7785.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+N = int(stdin.readline())
+co = dict()
+for _ in range(N):
+  name, door = map(str, stdin.readline().split())
+  if door == "enter":
+    co.update({name:True})
+  elif door=="leave":
+    co.update({name:False})
+
+co=dict(sorted(co.items(),reverse=True))
+
+for key in co.keys():
+  if co[key]==True:
+    print(key)
