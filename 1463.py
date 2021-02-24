b7373edb863dfd54f77ef186632d05db03968bfc
BecomeWeasel <hsw0194@gmail.com>
2021-02-24 11:31:16 +0900

    Upload february 24 #2 algorithm(dp)

---
+++ 1463.py
@@ -0,0 +1,23 @@
+from sys import stdin
+
+
+def ans():
+  dp = [0 for _ in range(N + 1)]
+
+  dp[1] = 0
+
+  for num in range(2, N + 1):
+    if num%6==0:
+      dp[num]=min(dp[num//2],dp[num//3],dp[num-1])+1
+    elif num%2==0:
+      dp[num]=min(dp[num//2],dp[num-1])+1
+    elif num%3==0:
+      dp[num]=min(dp[num//3],dp[num-1])+1
+    else:
+      dp[num]=dp[num-1]+1
+
+  return dp[N]
+
+
+N = int(stdin.readline())
+print(ans())
