f26908a76345aa345ac2ce2cf6efd931e0b1add0
BecomeWeasel <hsw0194@gmail.com>
2021-03-17 14:11:19 +0900

    Upload march 17 algorithm(implementation,adhoc)

---
+++ 4307.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+T = int(stdin.readline())
+
+
+def sol():
+  L, N = map(int, stdin.readline().split())
+  pos = []
+  for _ in range(N):
+    pos.append(int(stdin.readline()))
+
+  pos.sort()
+
+  min_time, max_time = 0, -1
+
+  for i in range(N):
+    min_time = max(min_time, min(pos[i], L - pos[i]))
+    max_time = max(max_time, max(pos[i], L - pos[i]))
+
+  return min_time, max_time
+
+
+for _ in range(T):
+  print(' '.join(map(str, sol())))
