46380e50f88180f5c6b0619124c6c951199eed87
BecomeWeasel <hsw0194@gmail.com>
2021-04-13 15:28:44 +0900

    Upload april 13 algorithm(BFS)

---
+++ programmers/43165_target_number.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def solution(numbers, target):
+  answer = 0
+  
+  q=deque()
+
+  q.append((-numbers[0],1))
+  q.append((numbers[0],1))
+
+  while len(q)!=0:
+    cumulative,count=q.popleft()
+
+    if count+1==len(numbers):
+      if cumulative+numbers[-1]==target:
+        answer+=1
+      elif cumulative-numbers[-1]==target:
+        answer+=1
+    else:
+      q.append((cumulative+numbers[count],count+1))
+      q.append((cumulative-numbers[count],count+1))
+  
+  
+
+  return answer
+
+
+if __name__ == "__main__":
+  print(solution([1, 1, 1, 1, 1], 3))
