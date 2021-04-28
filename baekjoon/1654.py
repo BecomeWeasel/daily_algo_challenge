f1ad23b654368843b314792a77265c62b53bd123
BecomeWeasel <hsw0194@gmail.com>
2021-04-28 15:30:07 +0900

    Upload april 28 #2 algorithm(easy binary search)

---
+++ baekjoon/1654.py
@@ -0,0 +1,32 @@
+from sys import stdin
+
+K, N = map(int, stdin.readline().split())
+
+
+def sol():
+    wires = []
+
+    for _ in range(K):
+        wires.append(int(stdin.readline()))
+
+    left, right = 1, max(wires)
+
+    answer = 0
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        count = 0
+
+        for wire in wires:
+            count += wire // mid
+
+        if count >= N:
+            answer = max(answer, mid)
+            left = mid + 1
+        else:
+            right = mid - 1
+    return answer
+
+print(sol())
