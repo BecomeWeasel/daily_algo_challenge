b78eb0ce62e38db4c0ea5ae63868cc776ff8d7c4
BecomeWeasel <hsw0194@gmail.com>
2021-06-07 14:49:38 +0900

    Upload june 1 algorithm(LIS)

---
+++ baekjoon/3745.py
@@ -0,0 +1,52 @@
+from sys import stdin
+
+
+def lis(arr):
+    if not arr:
+        return 0
+
+    # C[i] means smallest last number of lis subsequences whose length are i
+    INF = float('inf')
+    C = [INF for _ in range(len(arr) + 1)]
+    C[0] = -INF
+    # C[1] = arr[0]
+    C[1] = min(arr)
+    tmp_longest = 0
+
+    for n in arr:
+        if C[tmp_longest] < n:
+            tmp_longest += 1
+            C[tmp_longest] = n
+        else:
+            next_loc = search(1, tmp_longest, n, C)
+            C[next_loc] = n
+
+    return tmp_longest
+
+
+# Find i that matches C[i-1] < n <= C[i]
+def search(lo, hi, n, C):
+    if lo == hi:
+        return lo
+    # elif lo + 1 == hi:
+    # return lo if C[lo] >= n else hi
+
+    mid = (lo + hi) // 2
+    if C[mid] == n:
+        return mid
+    elif C[mid] < n:
+        return search(mid + 1, hi, n, C)
+    else:
+        return search(lo, mid, n, C)
+
+
+while True:
+
+    try:
+        N = int(input())
+
+        prices = list(map(int, stdin.readline().split()))
+
+        print(lis(prices))
+    except EOFError:
+        break
