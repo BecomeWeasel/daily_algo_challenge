82259c3494577d5ef858f6014b18327d4dc6281c
BecomeWeasel <hsw0194@gmail.com>
2021-06-17 18:29:48 +0900

    Upload june 17 algorithm(bfs)

---
+++ leetcode/559.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+from collections import deque
+
+class Solution:
+  def maxDepth(self, root: 'Node') -> int:
+    if not root:
+      return 0
+
+
+    if root.children:
+      return max(self.maxDepth(child) for child in root.children)+1
+    
+    else:
+        return 1
