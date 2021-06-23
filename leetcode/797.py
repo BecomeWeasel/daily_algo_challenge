96c597a7f870f6ee39b0c3118a748e01f77ba4b2
BecomeWeasel <hsw0194@gmail.com>
2021-06-23 10:19:32 +0900

    Upload june 23 algorithm(dfs)

---
+++ leetcode/797.py
@@ -0,0 +1,19 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        target=len(graph)-1
+        
+        result=[]
+        
+        def dfs(node,path):
+            if node==target:
+                result.append(path)
+            else:
+                for another_node in graph[node]:
+                    dfs(another_node,path+[another_node])
+                     
+        for adjacent_node in graph[0]:
+            dfs(adjacent_node,[0,adjacent_node])
+        
+        
+        return result
