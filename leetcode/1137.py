e91144b915da0282a2d6f7a85562c1140795f7a0
BecomeWeasel <hsw0194@gmail.com>
2021-06-29 09:45:56 +0900

    Upload june 29 #3 algorithm(easy dp)

---
+++ leetcode/1137.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp=[0]*38
+        
+        dp[1]=1
+        dp[2]=1
+        
+        for k in range(3,n+1):
+            dp[k]=dp[k-1]+dp[k-2]+dp[k-3]
+        
+        return dp[n]
+        
