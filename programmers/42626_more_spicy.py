c7929b8f40f99839519c09fa445203aca1797b0b
BecomeWeasel <hsw0194@gmail.com>
2021-06-30 19:39:58 +0900

    Upload june 30 #4 algorithm(heap)

---
+++ programmers/42626_more_spicy.py
@@ -0,0 +1,21 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    
+    heapq.heapify(scoville)
+    
+    while True:
+        # 이 방법이 min(scoville)<7 유지조건보다 빠름
+        first=heapq.heappop(scoville)
+        if first>=K:
+            break
+        if len(scoville)<1:
+            return -1
+        second=heapq.heappop(scoville)
+        
+        scoville.append(first+2*second)
+        # heapq.heappush(scoville,first+2*second)
+        answer+=1
+        
+    
+    return answer
