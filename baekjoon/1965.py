539b152c8add31a1d9227f79536239f3d164abc2
BecomeWeasel <hsw0194@gmail.com>
2021-06-30 22:03:49 +0900

    Upload june 30 #7 algorithm(LIS)

---
+++ baekjoon/1965.py
@@ -0,0 +1,25 @@
+from bisect import bisect_left
+from sys import stdin
+
+N=int(stdin.readline())
+
+def sol():
+    numbers=list(map(int,stdin.readline().split()))
+    INF=float('inf')
+
+    C=[-INF for _ in range(len(numbers)+1)]
+
+    C[1]=min(numbers)
+    lis_length=0
+
+    for n in numbers:
+        if C[lis_length]<n:
+            lis_length+=1
+            C[lis_length]=n
+        else:
+            loc=bisect_left(C,n,1,lis_length)
+            C[loc]=n
+    # print(C)
+    return lis_length
+
+print(sol())
