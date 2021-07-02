2fd11affa31a5d32a27a28711b08114f2be9e14f
BecomeWeasel <hsw0194@gmail.com>
2021-07-02 15:18:50 +0900

    Upload july 2 algorithm(stack)

---
+++ baekjoon/4949.py
@@ -0,0 +1,38 @@
+from sys import stdin
+
+
+while True:
+    s=stdin.readline().rstrip()
+
+    if s=='.':
+        break 
+    
+    
+
+    def valid(s,stack):
+        for char in s:
+            if char not in ['[',']','(',')']:
+                continue
+            elif char == ']' :
+                if len(stack)==0:
+                    return "no"
+                if stack[-1]=='[':
+                    stack.pop()
+                else:
+                    return "no"
+            elif char == ')':
+                if len(stack)==0:
+                    return "no"
+                if stack[-1]=='(':
+                    stack.pop()
+                else:
+                    return "no"
+            else:
+                stack.append(char)
+        if len(stack)!=0:
+            return "no"
+        else:
+            return "yes"
+    print(valid(s,list()))
+
+    
