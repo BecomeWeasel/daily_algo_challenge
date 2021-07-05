02a94b7a8d319d82e1b17d3c6c63c5b4ffc9026c
BecomeWeasel <hsw0194@gmail.com>
2021-07-05 18:27:40 +0900

    Upload july 5 algorithm(recursion)

---
+++ baekjoon/11729.py
@@ -0,0 +1,17 @@
+from sys import stdin
+import math
+
+K=int(stdin.readline())
+
+def sol():
+    print(int(math.pow(2,K))-1)
+    def move(a,b,n):
+        if n==1:
+            print(' '.join(map(str,[a,b])))
+            return
+        move(a,6-a-b,n-1)
+        print(' '.join(map(str,[a,b])))
+        move(6-a-b,b,n-1)
+    move(1,3,K)
+    
+sol()
