88d2deeab3354061d571f043c61d8c7ba8906c5e
BecomeWeasel <hsw0194@gmail.com>
2021-07-13 19:19:44 +0900

    Upload july 13 #4 algorithm(implementation,string)

---
+++ programmers/81031_number_and_char.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = ''
+
+    stack = []
+
+    para = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for c in s:
+        if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            answer += str(c)
+        else:
+            stack.append(c)
+
+            if ''.join(stack) in para:
+                answer += str(para.index(''.join(stack)))
+                stack = []
+
+    return int(answer)
