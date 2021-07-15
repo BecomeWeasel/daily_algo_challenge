4020db6478cf2fd782435909f4b40d88a6e46ec6
BecomeWeasel <hsw0194@gmail.com>
2021-07-15 16:02:31 +0900

    Upload july 15 algorithm(dp)

---
+++ baekjoon/1106.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+C, N = map(int, stdin.readline().split())
+
+
+def sol():
+    dp = [float('inf') for _ in range(2000)]
+
+    dp[0] = 0
+
+    cost, people = [], []
+    for _ in range(N):
+        c, p_ = map(int, stdin.readline().split())
+        cost.append(c)
+        people.append(p_)
+
+    for n in range(C + 1):
+        for c, p in zip(cost, people):
+            dp[n + p] = min(dp[n + p], dp[n] + c)
+
+    return min(dp[C:2000])
+
+
+print(sol())
---
+++ baekjoon/1535.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+N = int(stdin.readline())
+
+
+def sol():
+
+    dp = [[0 for _ in range(101)] for _ in range(20)]
+
+    L = list(map(int, stdin.readline().split()))
+    J = list(map(int, stdin.readline().split()))
+
+    for n in range(N):
+        for h in range(1,100 + 1):
+            if h - L[n] > 0:
+                dp[n][h] = max(dp[n - 1][h], dp[n - 1][h - L[n]] + J[n])
+            else:
+                if n==0:
+                    dp[0][h]=0
+                else:
+                    dp[n][h]=dp[n-1][h]
+
+    return max(dp[n])
+
+
+print(sol())
