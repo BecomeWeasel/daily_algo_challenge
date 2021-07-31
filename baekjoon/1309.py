2e6a9e86043f3a5cf336224d6804ce52a8a27855
BecomeWeasel <hsw0194@gmail.com>
2021-08-01 00:08:37 +0900

    Upload august 1 algorithm(dp)

--- baekjoon/1309.py
+++ baekjoon/1309.py
@@ -1,20 +1,18 @@
 from sys import stdin
 
+def sol():
+    dp=[[0,0,0] for _ in range(100001)]
 
-def ans():
-  dp[1][0] = 1
-  dp[1][1] = 1
-  dp[1][2] = 1
+    N=int(stdin.readline())
+    modulo=9901
 
-  for i in range(2, N + 1):
-    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % mod
-    dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod
-    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod
+    dp[1]=[1,1,1]
 
-  return (dp[N][0] + dp[N][1] + dp[N][2]) % mod
+    for n in range(2,N+1):
+        dp[n][0]=(dp[n-1][0]+dp[n-1][1]+dp[n-1][2])%modulo
+        dp[n][1]=(dp[n-1][0]+dp[n-1][2])%modulo
+        dp[n][2]=(dp[n-1][0]+dp[n-1][1])%modulo
 
+    return sum(dp[N])%9901
 
-mod = 9901
-N = int(stdin.readline())
-dp = [[0, 0, 0] for _ in range(N + 1)]
-print(ans())
+print(sol())
