15ac0568d60c305515bd57e05e7adf082c7b3ed0
BecomeWeasel <hsw0194@gmail.com>
2021-08-01 00:43:57 +0900

    Upload august 1 #2 algorithm(dp)

---
+++ baekjoon/2156.py
@@ -0,0 +1,22 @@
+from sys import stdin
+
+
+def sol():
+    N = int(stdin.readline())
+
+    wine = [-1]
+
+    for _ in range(N):
+        wine.append(int(stdin.readline()))
+
+    dp = [[0, 0, 0] for _ in range(N + 1)]
+
+    dp[1] = [0, wine[1], 0]
+    for i in range(2, N + 1):
+        dp[i][0] = max(dp[i - 1])
+        dp[i][1] = dp[i - 1][0] + wine[i]
+        dp[i][2] = dp[i - 1][1] + wine[i]
+
+    return max(dp[N])
+
+print(sol())
