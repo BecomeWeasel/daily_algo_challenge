23de4a6433c636f47e99617d036542da6235b7aa
BecomeWeasel <hsw0194@gmail.com>
2021-08-02 18:41:15 +0900

    Upload august 2 algorithm(dp)

---
+++ baekjoon/12026.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+
+def sol():
+    N = int(stdin.readline())
+
+    path=['X']+list(stdin.readline().rstrip())
+
+    dp = [float('inf') for _ in range(N + 1)]
+
+    dp[1]=0
+
+    for n in range(1,N):
+        for k in range(1,N):
+            if n+k>N:
+                break
+            if path[n]=='B' and path[n+k]=='O':
+                dp[n+k]=min(dp[n+k],dp[n]+k*k)
+            elif path[n]=='O' and path[n+k]=='J':
+                dp[n+k]=min(dp[n+k],dp[n]+k*k)
+            elif path[n]=='J' and path[n+k]=='B':
+                dp[n+k]=min(dp[n+k],dp[n]+k*k)
+
+    return dp[N] if dp[N]!=float('inf') else -1
+
+
+print(sol())
