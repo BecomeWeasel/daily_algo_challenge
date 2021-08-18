b753e575a192fd534d80e8c4bef75683e2ef6145
BecomeWeasel <hsw0194@gmail.com>
2021-08-18 16:36:52 +0900

    Upload august 18 #2 algorithm(dp)

---
+++ baekjoon/2631.py
@@ -0,0 +1,31 @@
+from sys import stdin
+from bisect import bisect_left
+
+
+def sol():
+    N = int(stdin.readline())
+
+    numbers = list()
+
+    for _ in range(N):
+        numbers.append(int(stdin.readline()))
+
+
+    C = [float('inf') for _ in range(N + 1)]
+    C[0] = -float('inf')
+    C[1] = numbers[0]
+
+    length = 1
+
+    for n in numbers:
+        if C[length] < n:
+            length += 1
+            C[length] = n
+        else:
+            loc = bisect_left(C, n)
+            C[loc] = n
+
+    return N-length
+
+
+print(sol())
