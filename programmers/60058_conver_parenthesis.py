9bc89a659da4a7fbcf64616135dedb9bd4795f58
BecomeWeasel <hsw0194@gmail.com>
2021-08-29 21:38:27 +0900

    Upload august 29 #2 algorithm(stack,string)

---
+++ programmers/60058_conver_parenthesis.py
@@ -0,0 +1,67 @@
+def is_valid(s):
+    stack = list()
+
+    for symbol in s:
+        if len(stack) == 0:
+            stack.append(symbol)
+            continue
+
+        if stack[-1] == '(' and symbol == ')':
+            stack.pop()
+            continue
+
+        stack.append(symbol)
+
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+def solution(p):
+    if is_valid(p):
+        return p
+
+    def solve(p):
+        # print("cehck")
+
+        if p == '':
+            return ''
+
+        if is_valid(p):
+            print("correct and return " + p)
+            return p
+
+        l_c, r_c = 0, 0
+
+        u, v = '', ''
+
+        for idx, symbol in enumerate(p):
+            if symbol == '(':
+                l_c += 1
+            else:
+                r_c += 1
+
+            if l_c == r_c:
+                u = p[:idx + 1]
+                v = p[idx + 1:]
+                break
+
+        if is_valid(u):
+            return u + solve(v)
+
+        temp = '(' + solve(v) + ')'
+
+        u = u[1:-1]
+        u = list(u)
+        for idx, symbol in enumerate(u):
+            if symbol == '(':
+                u[idx] = ')'
+            else:
+                u[idx] = '('
+        temp += ''.join(map(str, u))
+        return temp
+
+    answer = solve(p)
+
+    return answer
