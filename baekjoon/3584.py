97aa17ba1872bf4114888395f98193343fd93683
BecomeWeasel <hsw0194@gmail.com>
2022-07-21 19:48:38 +0900

    Upload july 21 #2 algorithm(LCA,dfs,tree)

--- baekjoon/3584.py
+++ baekjoon/3584.py
@@ -1,45 +1,38 @@
 from sys import stdin
 
 
-class node:
-    def __init__(self):
-        self.parent = -1
-
-    def set_parent(self, parent):
-        self.parent = parent
-
-
 T = int(stdin.readline())
 
 
-def ans(N, nodes, first, second):
-    firsts_parent = list()
+def sol(N):
+    visit = [False for _ in range(N + 1)]
 
-    aim = first
+    parent = [-1 for _ in range(N + 1)]
 
-    firsts_parent.append(first)
-
-    while nodes[aim].parent != -1:
-        firsts_parent.append(nodes[aim].parent)
-        aim = nodes[aim].parent
-
-    aim = second
-
-    while nodes[aim].parent != -1:
-        target = nodes[aim].parent
-
-        for parent in firsts_parent:
-            if target == parent:
-                return target
-        aim = target
-
-
-while T:
-    N = int(stdin.readline())
-    nodes = [node() for _ in range(N + 1)]
-    for i in range(N - 1):
+    for _ in range(N - 1):
         A, B = map(int, stdin.readline().split())
-        nodes[B].set_parent(A)
-    first, second = map(int, stdin.readline().split())
-    print(ans(N, nodes, first, second))
-    T -= 1
+
+        parent[B] = A
+
+    a, b = map(int, stdin.readline().split())
+
+    p, curr = parent[a], a
+    visit[a] = True
+    i = 0
+    while curr != -1:
+        # print(curr,p)
+        visit[curr] = True
+        p, curr = parent[p], parent[curr]
+
+    p, curr = parent[b], b
+
+    while not visit[curr]:
+        p, curr = parent[p], parent[curr]
+
+    return curr
+
+
+for _ in range(T):
+    N = int(stdin.readline())
+
+    print(sol(N))
